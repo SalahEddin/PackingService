be062cd3268e888ac88937a9b3ccc98b3439d0ee
Salah Eddin Alshaal <salshaal@gmail.com>
2020-03-15 15:29:00 +0100

    unit test imports files from other project

--- PackingService.Tests/UnitTest1.fs
+++ PackingService.Tests/UnitTest1.fs
@@ -2,7 +2,7 @@ module PackingServiceTests
 
 open System
 open NUnit.Framework
-open PackingService
+open PackingService.Filters
 
 [<SetUp>]
 let Setup () =
@@ -11,3 +11,9 @@ let Setup () =
 [<Test>]
 let Test1 () =
     Assert.Pass()
+
+[<Test>]
+let TestEvenSequence() =
+    let expected = [1; 1; 1; 1]
+    let actual = MyMath.squaresOfOdds [2; 1; 4; 1; 6; 1; 8; 1; 10]
+    Assert.That(actual, Is.EqualTo(expected))
