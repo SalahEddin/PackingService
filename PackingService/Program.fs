c5c945df740bb69b69d7b339af851d42b1938c52
Salah Eddin Alshaal <salshaal@gmail.com>
2020-03-22 11:02:18 +0100

    Remove deprecated code

--- PackingService/Program.fs
+++ PackingService/Program.fs
@@ -6,6 +6,7 @@ open Microsoft.AspNetCore.Cors.Infrastructure
 open Microsoft.AspNetCore.Hosting
 open Microsoft.Extensions.Logging
 open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Hosting
 open Giraffe
 open Giraffe.Serialization.Json
 open Newtonsoft.Json
@@ -49,7 +50,7 @@ let configureCors (builder : CorsPolicyBuilder) =
            |> ignore
 
 let configureApp (app : IApplicationBuilder) =
-    let env = app.ApplicationServices.GetService<IHostingEnvironment>()
+    let env = app.ApplicationServices.GetService<IWebHostEnvironment>()
     (match env.IsDevelopment() with
     | true  -> app.UseDeveloperExceptionPage()
     | false -> app.UseGiraffeErrorHandler errorHandler)
